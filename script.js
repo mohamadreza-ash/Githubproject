4619e5b3a276c49f3835bfa10cabf6ab32531448
Mohamadreza <mohamadrezaash23@gmail.com>
2025-10-24 02:03:11 +0330

    commit1

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("h1").addEventListener("mousemove",(e)=>{
+    e.target.classList.add("heading")
+})
+document.querySelector("h1").addEventListener("mouseleave",(e)=>{
+    document.querySelector("h1").classList.remove("heading")
+})
